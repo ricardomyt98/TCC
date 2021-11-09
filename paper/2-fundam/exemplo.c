924d41a5e2015ee43f771174d26f6a5bf58fd78e
Ricardo Norio Miyata <ricardomyt98@gmail.com>
2021-11-09 00:37:27 -0300

    Secure commit

--- paper/2-fundam/exemplo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main (int argc, char *argv[])
-{
-   int i ;                           // uma variável local
-
-   for (i=0; i< 100; i++)            // um laço for
-      printf ("i vale %d\n", i) ;    // uma saída na tela
-}
